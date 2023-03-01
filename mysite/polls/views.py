10630c0743330c7a2e99e4f0f960b8c02ccde26b
Marmin <marmin.dev@gmail.com>
2023-03-01 20:14:22 +0900

    chap03 write index and details templates and logics

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from polls.models import Question
 
-# Create your views here.
+
+def index(request):
+    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, qeustion_id):
+    question = get_object_or_404(Question, pk=qeustion_id)
+    return render(request, 'polls/detail.html', {'question': question})
