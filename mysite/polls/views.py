310be4a453262fad93fbd126aa54c0ce5eed5f71
Marmin <marmin.dev@gmail.com>
2023-03-01 21:35:07 +0900

    vote webservice implemeted

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from polls.models import Question
-
+from django.http import HttpResponseRedirect
+from polls.models import Question,Choice
+from django.urls import reverse
 
 def index(request):
     latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
@@ -8,6 +9,28 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 
-def detail(request, qeustion_id):
-    question = get_object_or_404(Question, pk=qeustion_id)
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # 설문 투표 폼을 다시 보여준다
+        return render(request, 'polls/detail.html',{
+            'question':question,
+            'error_message':"You didn't select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # POST 데이터를 정상적으로 처리하였으면,
+        # 항상 HTTPResponseRedirect를 반환하여 리다이렉션을 처리한다
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html',{'question':question})
