7bacfcb97b3d2fdb80c204451a3bf84368a5775e
Marmin <marmin.dev@gmail.com>
2023-03-03 14:39:50 +0900

    Chap 04 Django Functions

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from polls.models import Question, Choice
 
-admin.site.register(Question)
+# admin.site.register(Question)
+# admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,{'fields':['question_text']}),
+        ('Date Information',{'fields':['pub_date'],'classes':['collapse']})
+    ]
+    inlines = [ChoiceInline] # Choice 모댈 클래스 같이보기
+    list_display = ('question_text', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
